7f4defbd5cf85b965cc560eed4035ece42ad3551
ahmedragab15 <ragabahmed154@gmail.com>
2025-05-18 22:59:08 +0300

    deplay.1

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,12 +1,18 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://fullstack-todo-app-gamma.vercel.app"],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
